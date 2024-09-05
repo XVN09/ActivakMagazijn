258d149cfd5b45730782ac705df640974fc4d3fa
XVN09 <114179675+XVN09@users.noreply.github.com>
2024-09-05 13:48:00 +0200

    Initial Commit

---
+++ ActivakMagazijn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivakMagazijn
+//
+//  Created by Xander Van nuffel on 05/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ActivakMagazijnTests/ActivakMagazijnTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActivakMagazijnTests.swift
+//  ActivakMagazijnTests
+//
+//  Created by Xander Van nuffel on 05/09/2024.
+//
+
+import Testing
+@testable import ActivakMagazijn
+
+struct ActivakMagazijnTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
